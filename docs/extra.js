cc0dbee9ab7ab8adbc3e3ff9111fccfede0024a2
portaldangerousgoods <portaldangerousgoods@gmail.com>
2025-07-01 15:33:07 -0300

    Update extra.js

--- docs/extra.js
+++ docs/extra.js
@@ -3,13 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (toc) {
     const img = document.createElement("img");
-    img.src = "avião-dhl.gif";
+    img.src = "/dpdhl/avião-dhl.gif"; // <-- Caminho absoluto
     img.alt = "Avião DHL";
     img.style.width = "100px";
     img.style.display = "block";
     img.style.margin = "16px auto";
 
-    // Insere o GIF logo após o índice
     toc.parentElement.appendChild(img);
   }
 });
